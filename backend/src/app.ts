89b517d9b5ba0e98d77b02cd72945fbb41ca4ffa
Mahir4098 <mahirjr98@gmail.com>
2026-09-06 19:59:01 +0600

    Fix: mount all module routes in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,15 +7,17 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { globalErrorHandler } from "./middleware/globalErrorHandler";
 import { notFound } from "./middleware/notFound";
+import { authRoute } from "./module/auth/auth.routes";
 import { boardRoute } from "./module/board/board.route";
-
+import boardShareRoute from "./module/boardShare/boardShare.route";
+import columnRoute from "./module/column/column.routes";
+import taskRoute from "./module/task/task.route";
 
 export const app: Application = express();
 
 app.use(cors({ origin: true, credentials: true }));
 
-app.use("/api/subscription/webhook", express.raw({ type: 'application/json' }))
-
+app.use("/api/subscription/webhook", express.raw({ type: 'application/json' }));
 
 app.use(
   express.json({
@@ -28,11 +30,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+// Routes
+app.use("/api/auth", authRoute);
 app.use("/api/boards", boardRoute);
-
-
-
-
+app.use("/api/boards", boardShareRoute);
+app.use("/api", columnRoute);
+app.use("/api", taskRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome To The Kanban Server");
